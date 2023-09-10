cebb9fa4289bcacea80faa8bbede4ff6aa1e1d80
AlvaroWRP <62357109+AlvaroWRP@users.noreply.github.com>
2023-09-10 07:23:31 -0300

    teste com twilio

---
+++ EX8_SMS_twilio.py
@@ -0,0 +1,15 @@
+import EX8_SMS_twilio_info as info
+from twilio.rest import Client
+
+account_sid = info.account_sid
+auth_token = info.auth_token
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to=info.destination,
+    from_=info.sender,
+    body='oiiii',
+)
+
+print(message.sid)
