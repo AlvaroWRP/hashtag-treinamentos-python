ac74e7ca499d523ef4367d93cff3848d37ff4f11
AlvaroWRP <62357109+AlvaroWRP@users.noreply.github.com>
2023-09-25 23:44:31 -0300

    começo modulo analise de dados

---
+++ Analise de dados/A1_arquivos_csv.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+dataframe = pd.read_csv(r'Analise de Dados\Contoso - Cadastro Produtos.csv', sep=';')
+print(dataframe, '\n')
+print('*' * 50, '\n')
+
+dataframe = pd.read_csv(r'Analise de Dados\Contoso - Vendas - 2017.csv', sep=';')
+print(dataframe, '\n')
+print('*' * 50, '\n')
+
+# mostra a coluna escolhida por inteira
+print(dataframe['Quantidade Vendida'], '\n')
+print('*' * 50, '\n')
+
+# mostra as colunas escolhidas por inteiras, que devem ser passadas dentro de uma lista
+print(dataframe[['ID Produto', 'Quantidade Vendida', 'Quantidade Devolvida']], '\n')
+print('*' * 50, '\n')
+
+# mostra as linhas escolhidas por inteiras, tendo o mesmo funcionamento de uma lista normal
+print(dataframe['Quantidade Vendida'][1:11:2], '\n')
+print('*' * 50, '\n')
+
+# também funciona para o dataframe inteiro
+print(dataframe[:5], '\n')
+print('*' * 50, '\n')
+
+dataframe.info()
